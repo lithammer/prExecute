c9ce6a6f0a386b2c08ae506d3db4a55816551dd2
Peter Renström <renstrom.peter@gmail.com>
2010-02-05 01:45:55 +0100

    Fixed some spelling issues

--- prDrainSoulTimer.lua
+++ prDrainSoulTimer.lua
@@ -1,4 +1,4 @@
-if select(2, UnitClass('player')) == 'WARLOCK' then
+if select(2, UnitClass("player")) == "WARLOCK" then
 
 
 -- CONFIG -----------------------------------------------------------------------------------------------
@@ -12,12 +12,12 @@ local alwaysPlayTick = false	-- plays the drain soul tick sound for all specs
 
 
 local played = false
-local f = CreateFrame('Frame')
-f:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
-f:RegisterEvent('PLAYER_ENTERING_WORLD')
-if alwaysPlayTick then f:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED') end
+local f = CreateFrame("Frame")
+f:RegisterEvent("ACTIVE_TALENT_GROUP_CHANGED")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+if alwaysPlayTick then f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED") end
 
-f:SetScript('OnEvent', function(self, event, ...)
+f:SetScript("OnEvent", function(self, event, ...)
 	if self[event] then
 		return self[event] (self, event, ...)
 	end
@@ -27,15 +27,15 @@ function checkForTalent()
 	local rank, maxRank = select(5, GetTalentInfo(1, 24, false, false, nil))
 	
 	if rank == maxRank then
-		if not alwaysPlayTick then f:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED') end
-		f:RegisterEvent('UNIT_HEALTH')
-		f:RegisterEvent('PLAYER_TARGET_CHANGED')
+		if not alwaysPlayTick then f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED") end
+		f:RegisterEvent("UNIT_HEALTH")
+		f:RegisterEvent("PLAYER_TARGET_CHANGED")
 		
-		if notification then print ('|cff4e96f7|Hspell:47200|h[Death\'s Embrace]|h|r detected, activating |cffFF33FFp|rDrainSoulTimer') end
+		if notification then print ("|cff4e96f7|Hspell:47200|h[Death's Embrace]|h|r detected, activating |cffFF33FFpr|rDrainSoulTimer") end
 	else
-		if not alwaysPlayTick then f:UnregisterEvent('COMBAT_LOG_EVENT_UNFILTERED') end
-		f:UnregisterEvent('UNIT_HEALTH')
-		f:UnregisterEvent('PLAYER_TARGET_CHANGED')
+		if not alwaysPlayTick then f:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED") end
+		f:UnregisterEvent("UNIT_HEALTH")
+		f:UnregisterEvent("PLAYER_TARGET_CHANGED")
 	end
 end
 
@@ -51,26 +51,26 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
 	local eventType = select(2, ...)
 	local srcName = select(4, ...)
 	
-	if eventType == 'SPELL_PERIODIC_DAMAGE' and srcName == UnitName('player') then
+	if eventType == "SPELL_PERIODIC_DAMAGE" and srcName == UnitName("player") then
 		local spellName = select(10, ...)
 		local drainSoulName = GetSpellInfo(1120) --Drain Soul Rank 1
 
-		if(spellName == drainSoulName) then
-        	PlaySoundFile('Interface\\AddOns\\pDrainSoulTimer\\Sounds\\tick.wav')
+		if spellName == drainSoulName then
+        	PlaySoundFile("Interface\\AddOns\\prDrainSoulTimer\\Sounds\\tick.wav")
 		end
 	end
 end
 
 function f:UNIT_HEALTH()
-	if played or not UnitIsEnemy('player', 'target') or UnitIsDead('target') then
+	if played or not UnitIsEnemy("player", "target") or UnitIsDead("target") then
 		return
 	end
 	
-	local currentHealth = UnitHealth('target') / UnitHealthMax('target')
+	local currentHealth = UnitHealth("target") / UnitHealthMax("target")
 	
-	if UnitHealthMax('target') > minHealth then
+	if UnitHealthMax("target") > minHealth then
 		if currentHealth < 0.25 then
-			PlaySoundFile('Interface\\AddOns\\pDrainSoulTimer\\Sounds\\quaddamage.wav')
+			PlaySoundFile("Interface\\AddOns\\prDrainSoulTimer\\Sounds\\quaddamage.wav")
 
 			played = true
 		end
