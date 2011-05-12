3249979adfd1ff58cd622e78acdd8e5728ef6eef
ballagarba <renstrom.peter@gmail.com>
2011-05-12 19:46:34 +0200

    Definitely fixed now :)

--- prExecute.lua
+++ prExecute.lua
@@ -19,44 +19,15 @@ local playerName = UnitName('player')
 local warningSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\quaddamage.mp3'
 local tickSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\tick.mp3'
 
-function addon:ACTIVE_TALENT_GROUP_CHANGED()
-	checkForExecute()
-end
-
-local drainSoulName = GetSpellInfo(1120) --Drain Soul
-function addon:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
-	local _, eventType, _, _, srcName, _, _, _, _, _, spellName = ...
-
-	if eventType == 'SPELL_PERIODIC_DAMAGE' and srcName == playerName and spellName == drainSoulName then
-		PlaySoundFile(tickSound)
-	end
-end
-
-local executeRange = 0
-local soundPlayed = false
-function addon:UNIT_HEALTH(self, unit)
-	if (unit ~= 'target') or soundPlayed or CanExitVehicle() or UnitIsDeadOrGhost('target') or UnitIsFriend('player', 'target') then
-		return
-	end
-
-	local currentHealth = (UnitHealth('target') / UnitHealthMax('target')) * 100
-
-	if (UnitClassification('target') ~= 'normal') or UnitIsPlayer('target') then
-		if currentHealth < executeRange then
-			PlaySoundFile(warningSound)
-			soundPlayed = true
-		end
-	end
-end
-
-function addon:PLAYER_TARGET_CHANGED()
-	soundPlayed = false
+local function OnEvent(self, event, ...)
+	addon[event](self, event, ...)
 end
 
 local frame = CreateFrame('Frame')
 frame:RegisterEvent('PLAYER_ENTERING_WORLD')
 frame:SetScript('OnEvent', OnEvent)
 
+local executeRange = 0
 local function checkForExecute()
 	local hasExecute = ExecuteList[playerClass][GetPrimaryTalentTree()] or false
 
@@ -74,12 +45,42 @@ local function checkForExecute()
 	end
 end
 
+function addon:ACTIVE_TALENT_GROUP_CHANGED()
+	checkForExecute()
+end
+
 function addon:PLAYER_ENTERING_WORLD()
 	frame:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
 	frame:UnregisterEvent('PLAYER_ENTERING_WORLD')
 	checkForExecute()
 end
 
-local function OnEvent(self, event, ...)
-	addon[event](self, event, ...)
+local drainSoulName = GetSpellInfo(1120) --Drain Soul
+function addon:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
+	local _, eventType, _, _, srcName, _, _, _, _, _, spellName = ...
+
+	if eventType == 'SPELL_PERIODIC_DAMAGE' and srcName == playerName and spellName == drainSoulName then
+		PlaySoundFile(tickSound)
+	end
+end
+
+local soundPlayed = false
+function addon:UNIT_HEALTH(self, unit)
+	if (unit ~= 'target') or soundPlayed or CanExitVehicle() or UnitIsDeadOrGhost('target') or UnitIsFriend('player', 'target') then
+		return
+	end
+
+	local currentHealth = (UnitHealth('target') / UnitHealthMax('target')) * 100
+
+	if (UnitClassification('target') ~= 'normal') or UnitIsPlayer('target') then
+		
+		if currentHealth < executeRange then
+			PlaySoundFile(warningSound)
+			soundPlayed = true
+		end
+	end
+end
+
+function addon:PLAYER_TARGET_CHANGED()
+	soundPlayed = false
 end
