ce9e1ebda36edf786c0e790eb0d7c40763b3ebe9
Peter Renström <renstrom.peter@gmail.com>
2011-07-08 14:34:01 +0200

    Some refactoring

--- prExecute.lua
+++ prExecute.lua
@@ -1,48 +1,52 @@
-local addonName, addon = ...
 
+--[ CONFIG ]-------------------------------------------------------------------
+
+-- This is the specs from left to right, and the percentage value that the
+-- execute sound should play, 0 to disable.
 local ExecuteList = {
-	['DRUID'] = {nil, nil, nil},
-	['DEATHKNIGHT'] = {nil, nil, nil},
-	['MAGE'] = {nil, nil, nil},
-	['HUNTER'] = {20, 20, 20},
-	['PALADIN'] = {nil, nil, 20},
-	['PRIEST'] = {nil, nil, 25},
-	['ROGUE'] = {nil, nil, nil},
-	['SHAMAN'] = {nil, nil, nil},
-	['WARLOCK'] = {25, nil, 20},
-	['WARRIOR'] = {20, 20, nil},
+	['DEATHKNIGHT'] = { 0,  0,  0},
+	['DRUID'] =       { 0,  0,  0},
+	['HUNTER'] =      {20, 20, 20},
+	['MAGE'] =        { 0,  0,  0},
+	['PALADIN'] =     { 0,  0, 20},
+	['PRIEST'] =      { 0,  0, 25},
+	['ROGUE'] =       { 0,  0,  0},
+	['SHAMAN'] =      { 0,  0,  0},
+	['WARLOCK'] =     {25,  0, 20},
+	['WARRIOR'] =     {20, 20,  0},
 }
+
+-- Sound to play when you enter execute phase
+local warningSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\quaddamage.mp3'
+-- Tick sound for Drain Soul (Warlocks only)
+local tickSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\tick.mp3'
+
+---[ END CONFIG ]--------------------------------------------------------------
+
+local addonName, addon = ...
 
 local playerClass = select(2, UnitClass('player'))
 local playerName = UnitName('player')
 
-local warningSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\quaddamage.mp3'
-local tickSound = 'Interface\\AddOns\\'..addonName..'\\Sounds\\tick.mp3'
+local drainSoulName = GetSpellInfo(1120) --Drain Soul
+
+local executeRange = 0
+local soundPlayed = false
+
+-- Utility functions
+
+local function checkForExecute()
+	executeRange = ExecuteList[playerClass][GetPrimaryTalentTree()] or 0
+end
+
+local function isInvalidUnit()
+	return (unit ~= 'target') or soundPlayed or CanExitVehicle() or UnitIsDeadOrGhost('target') or UnitIsFriend('player', 'target')
+end
+
+-- Event handling
 
 local function OnEvent(self, event, ...)
 	addon[event](self, event, ...)
-end
-
-local frame = CreateFrame('Frame')
-frame:RegisterEvent('PLAYER_ENTERING_WORLD')
-frame:SetScript('OnEvent', OnEvent)
-
-local executeRange = 0
-local function checkForExecute()
-	local hasExecute = ExecuteList[playerClass][GetPrimaryTalentTree()] or false
-
-	if hasExecute then
-		executeRange = hasExecute
-		frame:RegisterEvent('UNIT_HEALTH')
-		frame:RegisterEvent('PLAYER_TARGET_CHANGED')
-	else
-		frame:UnregisterEvent('UNIT_HEALTH')
-		frame:UnregisterEvent('PLAYER_TARGET_CHANGED')
-	end
-
-	if playerClass == 'WARLOCK' then
-		frame:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
-	end
 end
 
 function addon:ACTIVE_TALENT_GROUP_CHANGED()
@@ -50,23 +54,15 @@ function addon:ACTIVE_TALENT_GROUP_CHANGED()
 end
 
 function addon:PLAYER_ENTERING_WORLD()
-	frame:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
-	frame:UnregisterEvent('PLAYER_ENTERING_WORLD')
 	checkForExecute()
 end
 
-local drainSoulName = GetSpellInfo(1120) --Drain Soul
-function addon:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
-	local _, eventType, _, _, srcName, _, _, _, _, _, _, _, spellName = ...
-
-	if eventType == 'SPELL_PERIODIC_DAMAGE' and srcName == playerName and spellName == drainSoulName then
-		PlaySoundFile(tickSound)
-	end
+function addon:PLAYER_TARGET_CHANGED()
+	soundPlayed = false
 end
 
-local soundPlayed = false
 function addon:UNIT_HEALTH(self, unit)
-	if (unit ~= 'target') or soundPlayed or CanExitVehicle() or UnitIsDeadOrGhost('target') or UnitIsFriend('player', 'target') then
+	if isInvalidUnit() then
 		return
 	end
 
@@ -81,6 +77,23 @@ function addon:UNIT_HEALTH(self, unit)
 	end
 end
 
-function addon:PLAYER_TARGET_CHANGED()
-	soundPlayed = false
+function addon:COMBAT_LOG_EVENT_UNFILTERED(event, ...)
+	local _, eventType, _, _, srcName, _, _, _, _, _, _, _, spellName = ...
+
+	if eventType == 'SPELL_PERIODIC_DAMAGE' and srcName == playerName and spellName == drainSoulName then
+		PlaySoundFile(tickSound)
+	end
 end
+
+-- Frame creation
+
+local f = Createf('f')
+f:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
+f:RegisterEvent('PLAYER_ENTERING_WORLD')
+f:RegisterEvent('PLAYER_TARGET_CHANGED')
+f:RegisterEvent('UNIT_HEALTH')
+if playerClass == 'WARLOCK' then
+	f:RegisterEvent('COMBAT_LOG_EVENT_UNFILTERED')
+end
+
+f:SetScript('OnEvent', OnEvent)
